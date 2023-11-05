705fbc8bdf1b2b4f2e462515a59190cb4c83308b
Александр Ф <al@Apollo.local>
2023-11-05 17:31:55 +0300

    sql corrections

--- db/db_installer.sh
+++ db/db_installer.sh
@@ -5,44 +5,46 @@ DB_USER_PASS=x
 su postgres <<EOF
 createdb  $DB_NAME;
 psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_USER_PASS';"
+psql -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
 
-psql -U postgres -d  $DB_NAME -c "CREATE TABLE IF NOT EXISTS user ( 
+
+psql -U postgres -d  $DB_NAME -c "CREATE TABLE IF NOT EXISTS users (
 	id							uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
 	username					VARCHAR(45),
 	pwd_hash					VARCHAR(64),
-	amusement_park_rating		FLOAT(3,2) DEFAULT 1.0,
-	aquarium_rating				FLOAT(3,2) DEFAULT 1.0,
-	art_gallery_rating			FLOAT(3,2) DEFAULT 1.0,
-	bar_rating					FLOAT(3,2) DEFAULT 1.0,
-	mosque_rating				FLOAT(3,2) DEFAULT 1.0,
-	movie_theater_rating		FLOAT(3,2) DEFAULT 1.0,
-	bowling_alley_rating		FLOAT(3,2) DEFAULT 1.0,
-	museum_rating				FLOAT(3,2) DEFAULT 1.0,
-	cafe_rating					FLOAT(3,2) DEFAULT 1.0,
-	night_club_rating			FLOAT(3,2) DEFAULT 1.0,
-	park_rating					FLOAT(3,2) DEFAULT 1.0,
-	casino_rating				FLOAT(3,2) DEFAULT 1.0,
-	cemetery_rating				FLOAT(3,2) DEFAULT 1.0,
-	church_rating				FLOAT(3,2) DEFAULT 1.0,
-	city_hall_rating			FLOAT(3,2) DEFAULT 1.0,
-	restaurant_rating			FLOAT(3,2) DEFAULT 1.0,
-	shopping_mall_rating		FLOAT(3,2) DEFAULT 1.0,
-	stadium_rating				FLOAT(3,2) DEFAULT 1.0,
-	synagogue_rating			FLOAT(3,2) DEFAULT 1.0,
-	tourist_attraction_rating	FLOAT(3,2) DEFAULT 1.0,
-	hindu_temple_rating			FLOAT(3,2) DEFAULT 1.0,
-	zoo_rating					FLOAT(3,2) DEFAULT 1.0
+	amusement_park_rating		NUMERIC(3,2) DEFAULT 1.0,
+	aquarium_rating				NUMERIC(3,2) DEFAULT 1.0,
+	art_gallery_rating			NUMERIC(3,2) DEFAULT 1.0,
+	bar_rating					NUMERIC(3,2) DEFAULT 1.0,
+	mosque_rating				NUMERIC(3,2) DEFAULT 1.0,
+	movie_theater_rating		NUMERIC(3,2) DEFAULT 1.0,
+	bowling_alley_rating		NUMERIC(3,2) DEFAULT 1.0,
+	museum_rating				NUMERIC(3,2) DEFAULT 1.0,
+	cafe_rating					NUMERIC(3,2) DEFAULT 1.0,
+	night_club_rating			NUMERIC(3,2) DEFAULT 1.0,
+	park_rating					NUMERIC(3,2) DEFAULT 1.0,
+	casino_rating				NUMERIC(3,2) DEFAULT 1.0,
+	cemetery_rating				NUMERIC(3,2) DEFAULT 1.0,
+	church_rating				NUMERIC(3,2) DEFAULT 1.0,
+	city_hall_rating			NUMERIC(3,2) DEFAULT 1.0,
+	restaurant_rating			NUMERIC(3,2) DEFAULT 1.0,
+	shopping_mall_rating		NUMERIC(3,2) DEFAULT 1.0,
+	stadium_rating				NUMERIC(3,2) DEFAULT 1.0,
+	synagogue_rating			NUMERIC(3,2) DEFAULT 1.0,
+	tourist_attraction_rating	NUMERIC(3,2) DEFAULT 1.0,
+	hindu_temple_rating			NUMERIC(3,2) DEFAULT 1.0,
+	zoo_rating					NUMERIC(3,2) DEFAULT 1.0
 );"
 
-psql -U postgres -d  $DB_NAME -c "CREATE TABLE IF NOT EXISTS place ( 
+psql -U postgres -d  $DB_NAME -c "CREATE TABLE IF NOT EXISTS places ( 
 	id							uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
 	place_id					TEXT NOT NULL,
 	types						TEXT NOT NULL
 );"
 
-psql -U postgres -d  $DB_NAME -c "CREATE TABLE IF NOT EXISTS users_place ( 
-	user_id						uuid NOT NULL REFERENCES user(id) ON DELETE CASCADE,
-	place_id					uuid NOT NULL REFERENCES place(id) ON DELETE CASCADE,
+psql -U postgres -d  $DB_NAME -c "CREATE TABLE IF NOT EXISTS users_places ( 
+	user_id						uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	place_id					uuid NOT NULL REFERENCES places(id) ON DELETE CASCADE,
 	like_mark					BOOLEAN NOT NULL,
 	visited_mark				BOOLEAN NOT NULL,
 	CONSTRAINT					user_place_pkey PRIMARY KEY(user_id, place_id)
